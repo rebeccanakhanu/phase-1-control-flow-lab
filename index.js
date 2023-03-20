70c514b8ad0ef6168c9b70f4ac9b875c1b504fd6
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-03-20 20:06:27 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return "No can do.";
+  }
 }
+function ternaryCheckCity(checkCity){
+  if (checkCity==='NYC'){
+    return "Ok, sounds good."
+  }
+  else if(checkCity!='NYC'){
+    return "No go."
 
-function ternaryCheckCity(){
-  // Write your code here!
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.';
+    case "not as generous":
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
